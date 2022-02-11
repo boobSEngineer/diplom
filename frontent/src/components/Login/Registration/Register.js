0ef359aba97765e6d848187b7edbb547196f61a0
Helen <helenz.eltech@gmail.com>
2022-02-11 17:44:49 +0300

    using react-hook-form for validation registration , input field

--- frontent/src/components/Login/Registration/Register.js
+++ frontent/src/components/Login/Registration/Register.js
@@ -3,12 +3,18 @@ import {useForm} from "react-hook-form";
 import s from "./Register.module.css";
 
 const Register = (props) => {
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, formState: {errors}} = useForm({mode: "onBlur"});
     const handleError = (errors) => {
     };
     const processForm = (d) => {
         props.register(d.login, d.username, d.password)
-    }
+    };
+
+    const registerOptions = {
+        login: {required: "login is required"},
+        username: {required: "username is required"},
+        password: {required: "password is required"},
+    };
 
     return (
         <div className={s.register_wrapper}>
@@ -17,17 +23,26 @@ const Register = (props) => {
                 <label>
                     <p>Login</p>
                     <input type="text" name="login" placeholder="login"
-                           {...register('login')}/>
+                           {...register('login', registerOptions.login)}/>
+                    <small className="text-danger">
+                        {errors?.login && errors.login.message}
+                    </small>
                 </label>
                 <label>
                     <p>Username</p>
                     <input type="text" name="username" placeholder="username"
-                           {...register('username')}/>
+                           {...register('username', registerOptions.username)}/>
+                    <small className="text-danger">
+                        {errors?.username && errors.username.message}
+                    </small>
                 </label>
                 <label>
                     <p>Password</p>
                     <input type="password" name="password" placeholder="password"
-                           {...register('password')}/>
+                           {...register('password', registerOptions.password)}/>
+                    <small className="text-danger">
+                        {errors?.password && errors.password.message}
+                    </small>
                 </label>
                 <div>
                     <button type="submit">Submit</button>
