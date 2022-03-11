67180559ee1f3a294d531dbe4cb79f1324baf0f5
Helen <helenz.eltech@gmail.com>
2022-03-11 21:29:46 +0300

    profile settings css

---
+++ frontent/src/components/Profile/ProfileSettings/ProfileSettings.js
@@ -0,0 +1,37 @@
+import React from "react";
+import p from "../Profile.module.css";
+
+const ProfileSettings = () => {
+    return (
+        <>
+            <div className={p.wrapper}>
+                <div className={p.box}>
+                    <div className={p.sidebar_block}>
+                        <div className={p.sidebar}>
+                            <div className={p.sidebar_block_title}>
+                                Мой аккаунт
+                            </div>
+                            <div className={p.sidebar_nav}>
+                                <ul>
+                                    <li><a href="/profile">Панель управления</a></li>
+                                    <li><a href="/profile_settings">Детали профиля</a></li>
+                                    <li><a href="/fonts_upload">Загрузить шрифт</a></li>
+                                    <li><a href="/my_fonts">Мои шрифты</a></li>
+                                    <li><a href="/liked">Понравилось</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={p.content_block}>
+                        <div className={p.content}>
+                                ffffff
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProfileSettings
