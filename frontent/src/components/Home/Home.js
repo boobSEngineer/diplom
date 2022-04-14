02f8e65e2f3df89d9c2555d8b11ee443bca8d66e
Helen <helenz.eltech@gmail.com>
2022-04-14 17:05:26 +0300

    panel control change margin and padding/ use tag select for sort by ...

--- frontent/src/components/Home/Home.js
+++ frontent/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import c from "./PanelControl.module.css";
 import b from "./Content.module.css";
-import {faArrowRotateRight} from "@fortawesome/free-solid-svg-icons";
+import {faArrowRotateRight, faCat, faFrog, faGhost, faPoo} from "@fortawesome/free-solid-svg-icons";
 import {faBars} from "@fortawesome/free-solid-svg-icons";
 import {faTableCellsLarge} from "@fortawesome/free-solid-svg-icons";
 import {faHeart} from "@fortawesome/free-solid-svg-icons";
@@ -11,6 +11,18 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const Home = (props) => {
     const [isShrunk, setShrunk] = useState(false);
     const [style, setStyle] = useState(false);
+    const [like, setLike] = useState(false);
+
+    useEffect(() => {
+        setLike((like) => {
+            if (props.isAuth === true) {
+                return like = true;
+            } else {
+                return like = false
+            }
+        })
+    }, [props.isAuth]);
+
 
     useEffect(() => {
         const handler = () => {
@@ -39,75 +51,102 @@ const Home = (props) => {
         return () => window.removeEventListener("scroll", handler);
     }, []);
 
+    function switchSort(variant) {
+        switch (variant) {
+            case "All":
+                return props.requestFonts()
+            case "1":
+                return props.sortByViews()
+            case "2":
+                return props.sortByLikes()
+            case "3":
+                return props.sortByData()
+            default:
+                return "";
+        }
+    }
+
     return (
         <>
-            <div className={c.wrapper + " " + c.sticky_wrapper + (isShrunk ? " " + c.sticky_wrapper_effect : "")}>
+            <div className={c.wrapper_panel + " " + c.sticky_wrapper + (isShrunk ? " " + c.sticky_wrapper_effect : "")}>
                 <div className={c.control_setting}>
-                    <div className={c.sort_fonts}>
-                        <select>
-                            <option value="All" selected="selected">Сортировать</option>
-                            <option value="">Тренд</option>
-                            <option value="">Самый популярный</option>
-                            <option value="">Новое</option>
-                            <option value="">Алфовит</option>
-                        </select>
-                    </div>
-                    <form className={c.sentence_font}>
-                        <div className={c.input_something}>
-                            <input placeholder="Введите что-нибудь"/>
-                        </div>
-                    </form>
-                    <div className={c.range_font}>
-                        <div className={c.font_px}>
-                            <select>
-                                <option value="All" selected="selected">0px</option>
-                                <option value="10px">5px</option>
-                                <option value="20px">20px</option>
-                                <option value="30px">30px</option>
-                                <option value="40px">40px</option>
+                    <div className={c.container}>
+                        <div className={c.sort_fonts}>
+                            <select onChange={(e)=>{switchSort(e.target.value); debugger}}>
+                                <option value="All" selected="selected">Сортировать</option>
+                                <option value="1">Тренд</option>
+                                <option value="2">Самый популярный</option>
+                                <option value="3">Новое</option>
                             </select>
                         </div>
-                        <input className={c.range} type="range"/>
-                        <div className={c.button_circle_padding_1}>
-                            <div className={c.button_circle}>
-                                <button className={c.imge} type="reset">
-                                    <FontAwesomeIcon icon={faArrowRotateRight}/>
-                                </button>
+                    </div>
+                    <div className={c.container}>
+                        <form className={c.sentence_font}>
+                            <div className={c.input_something}>
+                                <input placeholder="Введите что-нибудь"/>
+                            </div>
+                        </form>
+                    </div>
+                    <div className={c.container}>
+                        <div className={c.range_font}>
+                            <div className={c.font_px}>
+                                <select>
+                                    <option value="All" selected="selected">0px</option>
+                                    <option value="10px">5px</option>
+                                    <option value="20px">20px</option>
+                                    <option value="30px">30px</option>
+                                    <option value="40px">40px</option>
+                                </select>
+                            </div>
+                            <input className={c.range} type="range"/>
+                            <div className={c.button_circle_padding_1}>
+                                <div className={c.button_circle}>
+                                    <button className={c.imge} type="reset">
+                                        <FontAwesomeIcon icon={faArrowRotateRight}/>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className={c.initial_base}>
-                        <div className={c.button_circle_padding_2}>
-                            <div className={c.button_circle}  onClick={() => {setStyle(true)}} >
-                                <button className={style? c.imge_count : c.imge} >
-                                    <FontAwesomeIcon icon={faBars}/>
-                                </button>
+                    <div className={c.container}>
+                        <div className={c.initial_base}>
+                            <div className={c.button_circle_padding_2}>
+                                <div className={c.button_circle} onClick={() => {
+                                    setStyle(true)
+                                }}>
+                                    <button className={style ? c.imge_count : c.imge}>
+                                        <FontAwesomeIcon icon={faBars}/>
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                        <div className={c.button_circle_padding_2}>
-                                <div className={c.button_circle} onClick={() => {setStyle(false)}}>
-                                    <button className={style? c.imge : c.imge_count}>
+                            <div className={c.button_circle_padding_2}>
+                                <div className={c.button_circle} onClick={() => {
+                                    setStyle(false)
+                                }}>
+                                    <button className={style ? c.imge : c.imge_count}>
                                         <FontAwesomeIcon icon={faTableCellsLarge}/>
                                     </button>
                                 </div>
+                            </div>
                         </div>
                     </div>
-                    <div className={c.sort_language}>
-                        <select>
-                            <option value="All" selected="selected"> Язык</option>
-                            <option value="">Русский</option>
-                            <option value="">Английский</option>
-                        </select>
-                    </div>
+                    {/*<div className={c.sort_language}>*/}
+                    {/*    <select>*/}
+                    {/*        <option value="All" selected="selected"> Язык</option>*/}
+                    {/*        <option value="">Русский</option>*/}
+                    {/*        <option value="">Английский</option>*/}
+                    {/*    </select>*/}
+                    {/*</div>*/}
                 </div>
             </div>
+
             <div className={c.wrapper}>
                 <div className={b.content}>
-                    <div className={style? b.grid_row : b.grid_square}>
+                    <div className={style ? b.grid_row : b.grid_square}>
                         {
                             props.fonts.map(f =>
                                 <>
-                                    <div className={style? b.card_row : b.card_square}>
+                                    <div className={style ? b.card_row : b.card_square}>
                                         <div className={b.box}>
                                             <div className={b.box_top}>
                                                 <p className={b.grid_title}>{f.full_name}</p>
@@ -122,7 +161,7 @@ const Home = (props) => {
                                             <div className={b.box_bottom_block2}>
                                                     <span className={b.grid_views}><FontAwesomeIcon
                                                         icon={faEye}/> {f.views}</span>
-                                                <span className={b.grid_likes}><FontAwesomeIcon icon={faHeart}/> 000000</span>
+                                                <span className={like ? b.grid_likes : b.grid_likes_not_auth}><FontAwesomeIcon icon={faHeart}/> 000000</span>
                                             </div>
                                         </div>
                                     </div>
--- frontent/src/components/Home/HomeContainer.js
+++ frontent/src/components/Home/HomeContainer.js
@@ -3,23 +3,41 @@ import Home from "./Home";
 import {getFontsById} from "../../redux/select/fonts-selector";
 import {compose} from "redux";
 import {connect} from "react-redux";
-import {RequestFontsThunkCreate} from "../../redux/fonts-reducer";
+import {
+    RequestFontsThunkCreate,
+    SortFontsByDataThunkCreate,
+    SortFontsByLikeThunkCreate, SortFontsByViewsThunkCreate
+} from "../../redux/fonts-reducer";
+import {getAuth} from "../../redux/select/user-selector";
 
 const HomeContainer = (props) => {
     useEffect(() => {
         props.requestFonts()
     }, []);
 
-    return <Home fonts={props.fonts}/>
+    return <Home
+        fonts={props.fonts}
+        isAuth={props.isAuth}
+        sortByLikes={props.sortByLikes}
+        sortByData={props.sortByData}
+        sortByViews={props.sortByViews}
+        requestFonts={props.requestFonts}
+    />
 }
 
 const MapStateToProps = (state) => {
     return {
         fonts: getFontsById(state),
+        isAuth: getAuth(state),
     }
 };
 
 export default compose(
     connect(MapStateToProps,
-        { requestFonts:RequestFontsThunkCreate}),
+        {
+            requestFonts: RequestFontsThunkCreate,
+            sortByLikes: SortFontsByLikeThunkCreate,
+            sortByData: SortFontsByDataThunkCreate,
+            sortByViews: SortFontsByViewsThunkCreate
+        }),
 )(HomeContainer)
