afb33a97b55ecd0521f4890718929a46badf047a
Helen <helenz.eltech@gmail.com>
2022-04-15 12:38:07 +0300

    change back router sort  to switch

--- backend/router/sort.js
+++ backend/router/sort.js
@@ -4,20 +4,25 @@ import {authorize} from "../middleware/authorize.js";
 
 const router = express.Router();
 
-router.get("/by_views", async (req, res) => {
-    let fonts = await db.any(`SELECT * FROM font ORDER BY views DESC`);
-    res.json(fonts);
+router.get("/by", async (req, res) => {
+    let {sort, search} = req.query
+    let fonts = null;
+    switch (sort) {
+        case "views":
+            fonts = await db.any(`SELECT * FROM font ORDER BY views DESC`);
+            break
+        case "likes":
+            fonts = await db.any(`SELECT id_font, count(*) id_user FROM likes GROUP BY id_font ORDER BY id_user DESC`)
+            break
+        case "data":
+            fonts = await db.any(`SELECT * FROM font ORDER BY id_font DESC`);
+            break
+        default:
+            fonts = await db.any(`SELECT * FROM font`);
+    }
+    res.json(fonts)
 });
 
-router.get("/by_likes", async (req, res) => {
-    let fonts = await db.any(`SELECT id_font, count(*) id_user FROM likes GROUP BY id_font ORDER BY id_user DESC`)
-    res.json(fonts);
-});
-
-router.get("/by_data", async (req, res) => {
-    let fonts = await db.any(`SELECT * FROM font ORDER BY id_font DESC`);
-    res.json(fonts);
-});
 
 
 export default router;
